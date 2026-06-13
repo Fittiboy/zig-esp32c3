71757921e96d7e9564085797ccd60a587b78a056
Fitti <dev@fitti.io>
2026-06-13 11:01:31 +0200

    Who let the dogs out?

--- src/start.s
+++ src/start.s
@@ -4,30 +4,13 @@
 
 _start:
     la sp, _stack_top
-
-    la t0, _bss_start
-    la t1, _bss_end
-1:
-    # Zero out bss
-    bgeu t0, t1, 2f
-    sw   zero, 0(t0)
-    addi t0, t0, 4
-    j    1b
-
-2:
     # Personal heap pointer convention
     la s11, _heap_start
 
-    # Disable RTC Watchdog, so it doesn't
-    # reset the board.
-    li t0, 0x60008000 # Low-Power Management Base Address
-    li t1, 0x50d83aa1 # Magic value for disabling write protection
-    sw t1, 0xa8(t0)   # Disable write protection
-    sw zero, 0x90(t0) # Clear the two active WDTCONFIGO bits
-    sw zero, 0xa8(t0) # Re-enable write protection
+    la t0, _bss_start
+    la t1, _bss_end
 
-    # Disable flash-boot watchdog, to prevent the even
-    # faster resets than the RTC watchdog
+    # Disable flash-boot watchdog, to prevent resets
     li t0, 0x6001f000   # Timer Group 0 Base Address
     li t1, 0x50d83aa1   # Magic value for disabling write protection
     sw t1, 0x64(t0)     # Disable write protection
@@ -36,9 +19,35 @@ _start:
     #   WDT_EN               = 0
     #   WDT_FLASHBOOT_MOD_EN = 0
     li t1, 0x00400000
-    sw t1, 0x48(t0)     # TIMG_WDTCONFIG0_REG
+    sw t1, 0x48(t0)     # Disable watchdog
     sw zero, 0x64(t0)   # Re-enable write protection
 
+    # Disable RTC Watchdog, so it also
+    # doesn't reset the board.
+    li t0, 0x60008000   # Low-Power Management Base Address
+    li t1, 0x50d83aa1   # Magic value for disabling write protection
+    sw t1, 0xa8(t0)     # Disable write protection
+    sw zero, 0x90(t0)   # Clear the two active WDTCONFIGO bits
+    sw zero, 0xa8(t0)   # Re-enable write protection
+
+    # Another watchdog to disable, the analog-domain
+    # Super Watchdog (SWD)
+    # (Base address shared with RTC Watchdog)
+    # ... Where are all these dogs coming from?
+    li t1, 0x8f1d312a   # Same base address, different key!
+    sw t1, 0xb0(t0)     # Disable write protection
+    li t1, 0x40000000   # Bit 30 disables this dog
+    sw t1, 0xac(t0)     # Disable SWD
+    sw zero, 0xb0(t0)   # Re-enable write protection
+
+1:
+    # Zero out bss
+    bgeu t0, t1, 2f
+    sw   zero, 0(t0)
+    addi t0, t0, 4
+    j    1b
+
+2:
     call main
 
 3:
